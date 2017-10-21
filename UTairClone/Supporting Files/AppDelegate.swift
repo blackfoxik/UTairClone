df3edf28bde0a66f73a1fd0fbe4a0644ebde04d7
Anton Lisitsyn <lisitsyn.anton.a@gmail.com>
2017-10-21 06:19:34 +0300

    storyboards_stubs

---
+++ UTairClone/Supporting Files/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  UTairClone
+//
+//  Created by Anton on 21.10.17.
+//  Copyright © 2017 Anton. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
